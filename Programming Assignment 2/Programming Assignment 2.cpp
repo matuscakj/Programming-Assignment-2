88a1e87f11497a87c92a333371b68e38e9b79e68
James Matuscak <matuscakj@gmail.com>
2017-04-18 13:52:59 -0400

    Initial Commit

--- Programming Assignment 2/Programming Assignment 2.cpp
+++ Programming Assignment 2/Programming Assignment 2.cpp
@@ -1,11 +1,42 @@
 // Programming Assignment 2.cpp : Defines the entry point for the console application.
 //
-
 #include "stdafx.h"
-
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cassert>
+using namespace std;
 
 int main()
 {
+	ifstream file;
+	string fileName;
+	string line;
+	bool isValid;
+	string strLabel = "strings";
+	string algebraLabel = "algebra";
+	string setsLabel = "sets";
+	string boleanLabel = "boolean";
+
+	cout << " Enter a filename for expressions to be evaluated: " << endl;
+	getline(cin, fileName);
+	file.open(fileName.c_str());
+	
+	while (getline(file, line)) {
+		if (line.find("strings")) {
+			
+		}
+		if (line.find("algebra")) {
+
+		}
+		if (line.find("sets")) {
+
+		}
+		if (line.find("boolean")) {
+
+		}
+	}
+
     return 0;
 }
 
